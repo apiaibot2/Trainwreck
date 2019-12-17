3bef74664d6eccdbb42036c8787efa1d655442e0
vamsikrishnaimmandi <vamsikrishnaimmandi@gmail.com>
2019-12-17 15:09:43 +0530

    Adding EventStatus Project

---
+++ force-app/main/default/aura/Picklist/PicklistController.js
@@ -0,0 +1,22 @@
+({
+    myaction : function(component, event, helper)    {
+        console.log("in comp1 js");
+        var pickval=component.find("statuspicklist").get("v.value");
+        console.log("pickval="+pickval);
+         var action=component.get("c.call");
+        action.setParams({"picval":pickval});
+        action.setCallback(this,function(b){ 
+            var state = b.getState();
+            console.log(state);
+            if(state === 'SUCCESS')
+            {
+                console.log(b.getReturnValue())
+                var Toast=$A.get("e.force:showToast");
+                Toast.setParams({"title":"SUCCESS","type":'Success',"message":"Weather data displayed"});
+                Toast.fire();
+                component.set("v.getdetails",b.getReturnValue());
+            }
+        });
+        $A.enqueueAction(action);  
+    }
+})
